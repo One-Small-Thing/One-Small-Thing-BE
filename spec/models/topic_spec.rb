29d851b9e7c5c7a9afb9be84b448c7d003676972
Sarah Hall <swatson1994@yahoo.com>
2022-08-25 20:12:51 -0400

    Test: Add relationship tests, validationg for U_T, comment out factory

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
   describe "validations" do
-    it { should validate_presence_of :name }
+    it { should validate_presence_of(:name) }
+  end
+
+  describe "relationships" do
+    it { should have_many(:user_topics) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validations" do
-    it { should validate_presence_of :name }
-    it { should validate_presence_of :email }
-    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+  end
+
+  describe "relationships" do
+    it { should have_many(:user_topics) }
+    it { should have_many(:topics).through(:user_topics) }
   end
 end
