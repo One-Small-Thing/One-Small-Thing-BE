6c069833b6b57c3c7d267a6d48c03e4b00a96f52
Sarah Hall <swatson1994@yahoo.com>
2022-08-29 18:51:58 -0400

    Test: Fix missing period next to first expect

--- spec/facades/news_facade_spec.rb
+++ spec/facades/news_facade_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe NewsFacade, :vcr do
   it 'can make a news story rray from a service call' do
     stories = NewsFacade.create_general_stories
 
-    expect(stories)to be_an(Array)
+    expect(stories).to be_an(Array)
     expect(stories.first).to be_an_instance_of(Story)
     expect(stories.length).to be(10)
   end
