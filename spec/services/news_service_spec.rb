f4f6ddf57ddd19c8646227ebb41f4934f6cc9d8f
Sarah Hall <swatson1994@yahoo.com>
2022-08-30 19:35:17 -0400

    Test: Add tests for abortion rights and environmental topic news calls

--- spec/services/news_service_spec.rb
+++ spec/services/news_service_spec.rb
@@ -41,4 +41,32 @@ RSpec.describe NewsService, :vcr do
       expect(element[:source].keys).to include(:id, :name)
     end
   end
+
+  it 'can return information from a topic: Environmental call' do
+    hash = NewsService.get_environmental_news
+
+    expect(hash).to be_a(Hash)
+    expect(hash.keys).to include(:status, :totalResults, :articles)
+    expect(hash[:articles]).to be_an(Array)
+    hash[:articles].each do |element|
+      expect(element).to be_a(Hash)
+      expect(element.keys).to include(:source, :author, :title, :description, :url, :urlToImage, :publishedAt, :content)
+      expect(element[:source]).to be_a(Hash)
+      expect(element[:source].keys).to include(:id, :name)
+    end
+  end
+
+  it 'can return information from a topic: Abortion Rights call' do
+    hash = NewsService.get_abortionrights_news
+
+    expect(hash).to be_a(Hash)
+    expect(hash.keys).to include(:status, :totalResults, :articles)
+    expect(hash[:articles]).to be_an(Array)
+    hash[:articles].each do |element|
+      expect(element).to be_a(Hash)
+      expect(element.keys).to include(:source, :author, :title, :description, :url, :urlToImage, :publishedAt, :content)
+      expect(element[:source]).to be_a(Hash)
+      expect(element[:source].keys).to include(:id, :name)
+    end
+  end
 end
