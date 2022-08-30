cf21454f71f25e24ba1c44459575d5887f2634da
Sarah Hall <swatson1994@yahoo.com>
2022-08-30 19:22:59 -0400

    Feat: Add method for lgbtq topic service call with place holder keywords

--- app/services/news_service.rb
+++ app/services/news_service.rb
@@ -4,4 +4,11 @@ class NewsService < BaseService
     response = conn_news.get("top-headlines?country=us")
     get_json(response)
   end
+
+  def self.get_lgbtq_news
+    response = conn_news.get("everything") do |f|
+      f.params['q'] = 'LGBTQ+ Transgender "Gay Marriage"'
+    end
+    get_json(response)
+  end
 end
