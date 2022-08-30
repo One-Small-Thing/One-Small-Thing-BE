54de28db2e6765e2fa6f32aa7f405b9c0e107718
Sarah Hall <swatson1994@yahoo.com>
2022-08-30 19:59:48 -0400

    Test: Add tests for FAO comp, Domestic violence, and education calls

--- spec/services/news_service_spec.rb
+++ spec/services/news_service_spec.rb
@@ -69,4 +69,46 @@ RSpec.describe NewsService, :vcr do
       expect(element[:source].keys).to include(:id, :name)
     end
   end
+
+  it 'can return information from a topic: Education call' do
+    hash = NewsService.get_education_news
+
+    expect(hash).to be_a(Hash)
+    expect(hash.keys).to include(:status, :totalResults, :articles)
+    expect(hash[:articles]).to be_an(Array)
+    hash[:articles].each do |element|
+      expect(element).to be_a(Hash)
+      expect(element.keys).to include(:source, :author, :title, :description, :url, :urlToImage, :publishedAt, :content)
+      expect(element[:source]).to be_a(Hash)
+      expect(element[:source].keys).to include(:id, :name)
+    end
+  end
+
+  it 'can return information from a topic: FAO Computing call' do
+    hash = NewsService.get_fao_comp_news
+
+    expect(hash).to be_a(Hash)
+    expect(hash.keys).to include(:status, :totalResults, :articles)
+    expect(hash[:articles]).to be_an(Array)
+    hash[:articles].each do |element|
+      expect(element).to be_a(Hash)
+      expect(element.keys).to include(:source, :author, :title, :description, :url, :urlToImage, :publishedAt, :content)
+      expect(element[:source]).to be_a(Hash)
+      expect(element[:source].keys).to include(:id, :name)
+    end
+  end
+
+  it 'can return information from a topic: Domestic Violence call' do
+    hash = NewsService.get_domestic_violence_news
+
+    expect(hash).to be_a(Hash)
+    expect(hash.keys).to include(:status, :totalResults, :articles)
+    expect(hash[:articles]).to be_an(Array)
+    hash[:articles].each do |element|
+      expect(element).to be_a(Hash)
+      expect(element.keys).to include(:source, :author, :title, :description, :url, :urlToImage, :publishedAt, :content)
+      expect(element[:source]).to be_a(Hash)
+      expect(element[:source].keys).to include(:id, :name)
+    end
+  end
 end
