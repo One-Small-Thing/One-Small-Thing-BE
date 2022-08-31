7711524ba03f5c41d16dfc2e0965898742ee853c
Sarah Hall <swatson1994@yahoo.com>
2022-08-31 17:08:43 -0400

    DB: Add keywords column to topics and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_235959) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_210337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_235959) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "keywords"
   end
 
   create_table "user_topics", force: :cascade do |t|
