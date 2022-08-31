2aed32fff728986fbe2821e2efff53b277fa331f
Sarah Hall <swatson1994@yahoo.com>
2022-08-30 20:07:33 -0400

    Refactor: service methods to use topic call helper method

--- app/services/news_service.rb
+++ app/services/news_service.rb
@@ -5,52 +5,45 @@ class NewsService < BaseService
     get_json(response)
   end
 
-  def self.get_lgbtq_news
+  def self.get_topic_call(topic_key_words)
     response = conn_news.get("everything") do |f|
-      f.params['q'] = 'LGBTQ+ Transgender "Gay Marriage"'
+      f.params['q'] = topic_key_words
     end
     get_json(response)
+  end
+
+  def self.get_lgbtq_news
+    topic_key_words = 'LGBTQ+ Transgender "Gay Marriage"'
+    get_topic_call(topic_key_words)
   end
 
   def self.get_guncontrol_news
-    response = conn_news.get("everything") do |f|
-      f.params['q'] = 'shooting "mass shooting" "gun control" "school shooting"'
-    end
-    get_json(response)
+    topic_key_words = 'shooting "mass shooting" "gun control" "school shooting"'
+    get_topic_call(topic_key_words)
   end
 
   def self.get_environmental_news
-    response = conn_news.get("everything") do |f|
-      f.params['q'] = '"environmental disaster" "natural disaster" "oil spill" "climate change" "ecological disaster"'
-    end
-    get_json(response)
+    topic_key_words = '"environmental disaster" "natural disaster" "oil spill" "climate change" "ecological disaster"'
+    get_topic_call(topic_key_words)
   end
 
   def self.get_abortionrights_news
-    response = conn_news.get("everything") do |f|
-      f.params['q'] = 'abortion "Roe V. Wade" "abortion access" "planned parenthood"'
-    end
-    get_json(response)
+    topic_key_words = 'abortion "Roe V. Wade" "abortion access" "planned parenthood"'
+    get_topic_call(topic_key_words)
   end
 
   def self.get_education_news
-    response = conn_news.get("everything") do |f|
-      f.params['q'] = 'college "access to education" "student loan debt" "higher education" "literacy rates"'
-    end
-    get_json(response)
+    topic_key_words = 'college "access to education" "student loan debt" "higher education" "literacy rates"'
+    get_topic_call(topic_key_words)
   end
 
   def self.get_fao_comp_news
-    response = conn_news.get("everything") do |f|
-      f.params['q'] = '"free and open computing" linux gnu kde gnome'
-    end
-    get_json(response)
+    topic_key_words = '"free and open computing" linux gnu kde gnome'
+    get_topic_call(topic_key_words)
   end
 
   def self.get_domestic_violence_news
-    response = conn_news.get("everything") do |f|
-      f.params['q'] = '"domestic violence survivor resource" "domestic violence" "domestic disturbance"'
-    end
-    get_json(response)
+    topic_key_words = '"domestic violence survivor resource" "domestic violence" "domestic disturbance"'
+    get_topic_call(topic_key_words)
   end
 end
