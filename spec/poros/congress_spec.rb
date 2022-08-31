a3a6dd23845e7a47de3d77cbfa51302b196c43a9
Matt Deming <vballmatt44@gmail.com>
2022-08-31 15:44:53 -0600

    Pull out prys for CircleCI to run

--- spec/poros/congress_spec.rb
+++ spec/poros/congress_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 RSpec.describe Congress do
   it 'exists and has attributes' do
     json = File.read('./spec/fixtures/dummy_files/congress_member_example.json')
-    binding.pry
     hash = JSON.parse(json, symbolize_names: true)
-    binding.pry
     member = Congress.new(hash)
 
     expect(member).to be_an_instance_of(Congress)
