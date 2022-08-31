c7a3c0240fcc0f1326115f76189a87d8e8cfa8e4
Sarah Hall <swatson1994@yahoo.com>
2022-08-30 20:18:33 -0400

    Test: Add tests for story array return from lgbtq, gc, and env facade

--- spec/facades/news_facade_spec.rb
+++ spec/facades/news_facade_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe NewsFacade, :vcr do
-  it 'can make a news story rray from a service call' do
+  it 'can make a news story array from a general stories service call' do
+    stories = NewsFacade.create_general_stories
+
+    expect(stories).to be_an(Array)
+    expect(stories.first).to be_an_instance_of(Story)
+    expect(stories.length).to be(10)
+  end
+
+  it 'can make a news story array from an LGBTQ news service call' do
+    stories = NewsFacade.create_LGBTQ_stories
+
+    expect(stories).to be_an(Array)
+    expect(stories.first).to be_an_instance_of(Story)
+    expect(stories.length).to be(10)
+  end
+
+  it 'can make a news story array from a Gun Control news service call' do
+    stories = NewsFacade.create_guncontrol_stories
+
+    expect(stories).to be_an(Array)
+    expect(stories.first).to be_an_instance_of(Story)
+    expect(stories.length).to be(10)
+  end
+
+  it 'can make a news story array from a Environmental news service call' do
     stories = NewsFacade.create_general_stories
 
     expect(stories).to be_an(Array)
