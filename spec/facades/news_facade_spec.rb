8920851dbd42b18de02b7d51c6929ea08c06fea6
Sarah Hall <swatson1994@yahoo.com>
2022-08-31 17:29:56 -0400

    Test: Refactor tests to use different, cleaner method

--- spec/facades/news_facade_spec.rb
+++ spec/facades/news_facade_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe NewsFacade, :vcr do
+  before(:each) do
+    gun_violence = Topic.create!( name: 'Gun Violence', keywords: 'shooting "mass shooting" "gun control" "school shooting"')
+    environmental = Topic.create!( name: 'Environmental', keywords: '"environmental disaster" OR "natural disaster" OR "oil spill" OR "climate change" OR "ecological disaster"')
+    lgbtq = Topic.create!( name: 'LGBTQ+', keywords: 'LGBTQ+ Transgender "Gay Marriage"')
+    abortion_rights = Topic.create!( name: 'Abortion Rights', keywords: 'abortion "Roe V. Wade" OR "abortion access" OR "planned parenthood"')
+    education = Topic.create!( name: 'Education', keywords: 'college OR "access to education" OR "student loan debt" OR "higher education" OR "literacy rates"')
+    domestic_violence = Topic.create!( name: 'Domestic Violence', keywords: '"domestic violence survivor resource" OR "domestic violence" OR "domestic disturbance"')
+    computing = Topic.create!( name: 'Free and Open Computing', keywords: '"free and open computing" OR linux OR gnu kde OR gnome')
+  end
   it 'can make a news story array from a general stories service call' do
     stories = NewsFacade.create_general_stories
 
@@ -9,59 +18,15 @@ RSpec.describe NewsFacade, :vcr do
     expect(stories.length).to be(10)
   end
 
-  it 'can make a news story array from an LGBTQ news service call' do
-    stories = NewsFacade.create_LGBTQ_stories
+  it 'can make a news story array from a topical news service call' do
+    array = ['Gun Violence', 'Environmental', 'LGBTQ+', 'Abortion Rights', 'Education', 'Domestic Violence', 'Free and Open Computing']
 
-    expect(stories).to be_an(Array)
-    expect(stories.first).to be_an_instance_of(Story)
-    expect(stories.length).to be(10)
-  end
+    array.each do |topic|
+      stories = NewsFacade.create_topic_stories(topic)
 
-  it 'can make a news story array from a Gun Control news service call' do
-    stories = NewsFacade.create_guncontrol_stories
-
-    expect(stories).to be_an(Array)
-    expect(stories.first).to be_an_instance_of(Story)
-    expect(stories.length).to be(10)
-  end
-
-  it 'can make a news story array from an Environmental news service call' do
-    stories = NewsFacade.create_environmental_stories
-
-    expect(stories).to be_an(Array)
-    expect(stories.first).to be_an_instance_of(Story)
-    expect(stories.length).to be(10)
-  end
-
-  it 'can make a news story array from an abortion rights news service call' do
-    stories = NewsFacade.create_abortionrights_stories
-
-    expect(stories).to be_an(Array)
-    expect(stories.first).to be_an_instance_of(Story)
-    expect(stories.length).to be(10)
-  end
-
-  it 'can make a news story array from an Education news service call' do
-    stories = NewsFacade.create_education_stories
-
-    expect(stories).to be_an(Array)
-    expect(stories.first).to be_an_instance_of(Story)
-    expect(stories.length).to be(10)
-  end
-
-  it 'can make a news story array from a FAO computing news service call' do
-    stories = NewsFacade.create_fao_comp_stories
-
-    expect(stories).to be_an(Array)
-    expect(stories.first).to be_an_instance_of(Story)
-    expect(stories.length).to be(10)
-  end
-
-  it 'can make a news story array from a Domestic Violence news service call' do
-    stories = NewsFacade.create_domestic_violence_stories
-
-    expect(stories).to be_an(Array)
-    expect(stories.first).to be_an_instance_of(Story)
-    expect(stories.length).to be(10)
+      expect(stories).to be_an(Array)
+      expect(stories.first).to be_an_instance_of(Story)
+      expect(stories.length).to be(10)
+    end
   end
 end
