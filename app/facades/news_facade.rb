4e54016380138c4fe934a66a8edab38576f678ee
Sarah Hall <swatson1994@yahoo.com>
2022-08-31 17:35:02 -0400

    Refactor: Update service/facade methods to condense indiv calls into one

--- app/facades/news_facade.rb
+++ app/facades/news_facade.rb
@@ -5,38 +5,8 @@ class NewsFacade
     json[:articles][0..9].map { |hash| Story.new(hash) }
   end
 
-  def self.create_LGBTQ_stories
-    json = NewsService.get_lgbtq_news
-    json[:articles][0..9].map { |hash| Story.new(hash) }
-  end
-
-  def self.create_guncontrol_stories
-    json = NewsService.get_guncontrol_news
-    json[:articles][0..9].map { |hash| Story.new(hash) }
-  end
-
-  def self.create_environmental_stories
-    json = NewsService.get_environmental_news
-    json[:articles][0..9].map { |hash| Story.new(hash) }
-  end
-
-  def self.create_abortionrights_stories
-    json = NewsService.get_abortionrights_news
-    json[:articles][0..9].map { |hash| Story.new(hash) }
-  end
-
-  def self.create_fao_comp_stories
-    json = NewsService.get_fao_comp_news
-    json[:articles][0..9].map { |hash| Story.new(hash) }
-  end
-
-  def self.create_education_stories
-    json = NewsService.get_education_news
-    json[:articles][0..9].map { |hash| Story.new(hash) }
-  end
-
-  def self.create_domestic_violence_stories
-    json = NewsService.get_domestic_violence_news
+  def self.create_topic_stories(topic)
+    json = NewsService.get_topic_call(topic)
     json[:articles][0..9].map { |hash| Story.new(hash) }
   end
 end
--- app/services/news_service.rb
+++ app/services/news_service.rb
@@ -5,45 +5,11 @@ class NewsService < BaseService
     get_json(response)
   end
 
-  def self.get_topic_call(topic_key_words)
+  def self.get_topic_call(topic_name)
+    query_topic = Topic.find_by(name: topic_name)
     response = conn_news.get("everything") do |f|
-      f.params['q'] = topic_key_words
+      f.params['q'] = query_topic.keywords
     end
     get_json(response)
-  end
-
-  def self.get_lgbtq_news
-    topic_key_words = 'LGBTQ+ Transgender "Gay Marriage"'
-    get_topic_call(topic_key_words)
-  end
-
-  def self.get_guncontrol_news
-    topic_key_words = 'shooting "mass shooting" "gun control" "school shooting"'
-    get_topic_call(topic_key_words)
-  end
-
-  def self.get_environmental_news
-    topic_key_words = '"environmental disaster" OR "natural disaster" OR "oil spill" OR "climate change" OR "ecological disaster"'
-    get_topic_call(topic_key_words)
-  end
-
-  def self.get_abortionrights_news
-    topic_key_words = 'abortion "Roe V. Wade" "abortion access" "planned parenthood"'
-    get_topic_call(topic_key_words)
-  end
-
-  def self.get_education_news
-    topic_key_words = 'college OR "access to education" OR "student loan debt" OR "higher education" OR "literacy rates"'
-    get_topic_call(topic_key_words)
-  end
-
-  def self.get_fao_comp_news
-    topic_key_words = '"free and open computing" OR linux OR gnu kde OR gnome'
-    get_topic_call(topic_key_words)
-  end
-
-  def self.get_domestic_violence_news
-    topic_key_words = '"domestic violence survivor resource" OR "domestic violence" OR "domestic disturbance"'
-    get_topic_call(topic_key_words)
   end
 end
