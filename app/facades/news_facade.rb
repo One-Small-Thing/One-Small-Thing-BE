ecbc5c675d1709bd7ca80290b003c8d265e5b5b7
Sarah Hall <swatson1994@yahoo.com>
2022-08-30 20:19:32 -0400

    Feat: Add facade methods for lgbtq, gc and env topics

--- app/facades/news_facade.rb
+++ app/facades/news_facade.rb
@@ -4,4 +4,19 @@ class NewsFacade
     json = NewsService.get_general_news
     json[:articles][0..9].map { |hash| Story.new(hash) }
   end
+
+  def self.create_LGBTQ_stories
+    json = NewsService.get_lgbtq_news
+    json[:articles][0..9].map { |hash| Story.new(hash) }
+  end
+
+  def self.create_guncontrol_stories
+    json = NewsService.get_guncontrol_news
+    json[:articles][0..9].map { |hash| Story.new(hash) }
+  end
+
+  def self.create_environmental_stories
+    json = NewsService.get_environmental_news
+    json[:articles][0..9].map { |hash| Story.new(hash) }
+  end
 end
