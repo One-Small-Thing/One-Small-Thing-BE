a94466c99affa8f1ce9c710f3a279361a08b0a5b
Colin Reinhart <colinbreinhartgmail.com>
2022-09-01 15:58:42 -0600

    Update User Mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :user_update, mutation: Mutations::UserUpdate
     field :user_delete, mutation: Mutations::UserDelete
     field :create_user, mutation: Mutations::CreateUser
   end
