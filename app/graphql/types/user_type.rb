d0aa1928ed3e76abb8b9977cecc61db94699b48d
Stirling Hostetter <stirlhoss@proton.me>
2022-09-01 15:42:35 -0500

    Add location field to user

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,10 +5,10 @@ module Types
     field :id, ID, null: true
     field :name, String, null: true
     field :email, String, null: false
+    field :location, String, null: false
     field :topics, [TopicType], null: true
     field :find_topics, Types::TopicType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-
   end
 end
