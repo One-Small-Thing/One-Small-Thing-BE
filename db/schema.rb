193cec5f8b49b1ed4a0c9c7781fb36f28799c560
Stirling Hostetter <stirlhoss@proton.me>
2022-09-01 15:43:33 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_210337) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
