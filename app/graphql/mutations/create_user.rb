c389b16bb9ad7a5bb75a053ff82775c0c3f5c3ad
Stirling Hostetter <stirlhoss@proton.me>
2022-09-01 16:07:30 -0500

    Made location deafault to CO and remvoed from mutation

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -2,13 +2,12 @@ module Mutations
   class CreateUser < Mutations::BaseMutation
     argument :name, String, required: true
     argument :email, String, required: true
-    argument :location, String, required: true
 
     field :user, Types::UserType, null: false
     field :errors, [String], null: false
 
-    def resolve(name:, email:, location:)
-      user = User.new(name: name, email: email, location: location)
+    def resolve(name:, email:)
+      user = User.new(name: name, email: email)
       if user.save
         {
           user: user,
