24186657c17c5d5e30941763586b44b9b93e04e6
Colin Reinhart <colinbreinhartgmail.com>
2022-09-02 16:46:51 -0600

    Expanded story search to 20 and excluded 3 news sources

--- app/facades/news_facade.rb
+++ app/facades/news_facade.rb
@@ -2,11 +2,11 @@ class NewsFacade
 
   def self.create_general_stories
     json = NewsService.get_general_news
-    json[:articles][0..9].map { |hash| Story.new(hash) }
+    json[:articles].map { |hash| Story.new(hash) }
   end
 
   def self.create_topic_stories(topic)
     json = NewsService.get_topic_call(topic)
-    json[:articles][0..9].map { |hash| Story.new(hash) }
+    json[:articles].map { |hash| Story.new(hash) }
   end
 end
