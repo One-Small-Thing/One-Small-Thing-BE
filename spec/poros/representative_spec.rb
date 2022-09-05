44147b3cbd7168eeecf7d3881aef6c722325c531
Colin Reinhart <colinbreinhartgmail.com>
2022-09-05 16:07:10 -0600

    Add poro test for representatives

--- spec/poros/representative_spec.rb
+++ spec/poros/representative_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Representative, :vcr do
   it "exists and has attributes" do
-    # address = "Colorado"
-    # rep = RepresentativeFacade.find_each_rep(address)
-    #
-    # require "pry"; binding.pry
+    address = "Colorado"
+    rep = RepresentativeFacade.find_each_rep(address)
+
+    expect(rep[0][:name]).to eq("Joseph R. Biden")
+    expect(rep[0][:party]).to eq("Democratic Party")
+    expect(rep[0][:office]).to eq("President of the United States")
   end
 end
